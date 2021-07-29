78d3caf302b740a88b3ce48a834d6fcd2e5d9ee0
Paul Khuong <pkhuong@backtrace.io>
2021-07-29 00:26:50 -0400

    verneuil/verneuil-restore: example binary to restore db snapshots
    Summary:
    We rely on the invoker to download the directory blob they want
    from the directory bucket: they may want to be judicious about
    which version of the data they want.  Given that blob and the
    replication configuration, we can easily download everything,
    and output the concatenated result to a new sqlite db file.
    
    Smoke test:
    
    ```
    pkhuong@yolo01:~$ time ./verneuil-restore 042a-verneuil[...]78b3%2F%2Fetc%2Fcoronerd%2Fcoronerd.db -o foo.db --config @verneuil.json  # recreate a 4.8 MB db file
    
        real    0m0.453s
        user    0m0.057s
        sys     0m0.053s
    pkhuong@yolo01:~$ md5sum foo.db
    decc68db7858336398a1c2e08d52da3a  foo.db
    pkhuong@yolo01:~$ sudo md5sum /etc/coronerd/coronerd.db
    decc68db7858336398a1c2e08d52da3a  /etc/coronerd/coronerd.db
    ```
    
    Help message:
    ```
    pkhuong@penguin:~/backtrace/backtrace-backend/src/rust/verneuil$ ../target/release/examples/verneuil-restore --help
    verneuil-restore 0.1.0
    The verneuil-restore utility accepts the path to a verneuil directory file, and reconstructs its contents to the `--out`
    argument (or stdout by default).
    
    In order to recover the database's contents, verneuil-restore must know where the writer was configured to upload its
    replication data.  By default, verneuil-restore looks for a configuration string (either "@/path/to/file.json" or actual
    JSON) in the `VERNEUIL_CONFIG` environment variable; this can be overridden with the `--config` flag.
    
    USAGE:
        verneuil-restore [OPTIONS] <directory>
    
    FLAGS:
        -h, --help
                Prints help information
    
        -V, --version
                Prints version information
    
    OPTIONS:
        -c, --config <config>
                The Verneuil JSON configuration used when originally copying the database to remote storage.
    
                A value of the form "@/path/to/json.file" refers to the contents of that file; otherwise, the argument
                itself is the configuration string.
    
                This parameter is optional, and defaults to the value of the `VERNEUIL_CONFIG` environment variable.
        -l, --log <log>
                Log level, in the same format as `RUST_LOG`.  Defaults to only logging errors to stderr; `--log=info`
                increases the verbosity to also log info and warning to stderr.
    
                To fully disable logging, pass `--log=off`.
        -o, --out <out>
                The path to the reconstructed output file.
    
                Defaults to stdout.
    
    ARGS:
        <directory>
                The directory file that describes the snapshot to restore.
    
                These are typically stored as objects in versioned buckets; it is up to the invoker to fish out the relevant
                version.
    ```
    
    Initial step towards making Verneuil useful for ops (part of Txxxx).
    
    Test Plan: Ran on yolo.
    
    Reviewers: nhendrickson, ahicks, wandrews
    
    Reviewed By: wandrews
    
    Differential Revision: https://foo.backtrace.io/Dxxxx

---
+++ examples/verneuil-restore.rs
@@ -0,0 +1,133 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+use verneuil::chain_error;
+use verneuil::fresh_error;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "verneuil-restore",
+    about = "Rebuilds a db snapshot from a verneuil directory file."
+)]
+/// The verneuil-restore utility accepts the path to a verneuil
+/// directory file, and reconstructs its contents to the `--out`
+/// argument (or stdout by default).
+///
+/// In order to recover the database's contents, verneuil-restore must
+/// know where the writer was configured to upload its replication
+/// data.  By default, verneuil-restore looks for a configuration
+/// string (either "@/path/to/file.json" or actual JSON) in the
+/// `VERNEUIL_CONFIG` environment variable; this can be overridden
+/// with the `--config` flag.
+struct Opt {
+    /// The directory file that describes the snapshot to restore.
+    ///
+    /// These are typically stored as objects in versioned buckets;
+    /// it is up to the invoker to fish out the relevant version.
+    #[structopt(parse(from_os_str))]
+    directory: PathBuf,
+
+    /// The Verneuil JSON configuration used when originally copying
+    /// the database to remote storage.
+    ///
+    /// A value of the form "@/path/to/json.file" refers to the
+    /// contents of that file; otherwise, the argument itself is the
+    /// configuration string.
+    ///
+    /// This parameter is optional, and defaults to the value of the
+    /// `VERNEUIL_CONFIG` environment variable.
+    #[structopt(short, long)]
+    config: Option<String>,
+
+    /// Log level, in the same format as `RUST_LOG`.  Defaults to
+    /// only logging errors to stderr; `--log=info` increases the
+    /// verbosity to also log info and warning to stderr.
+    ///
+    /// To fully disable logging, pass `--log=off`.
+    #[structopt(short, long)]
+    log: Option<String>,
+
+    /// The path to the reconstructed output file.
+    ///
+    /// Defaults to stdout.
+    #[structopt(short, long, parse(from_os_str))]
+    out: Option<PathBuf>,
+}
+
+pub fn main() -> verneuil::Result<()> {
+    use prost::Message;
+    use tracing_subscriber::EnvFilter;
+
+    let opts = Opt::from_args();
+
+    // Send tracing calls to stdout, and converts any log! call to
+    // traces.
+    let filter = if let Some(log_level) = &opts.log {
+        EnvFilter::try_new(log_level)
+    } else {
+        Ok(EnvFilter::from_default_env())
+    }
+    .expect("failed to parse --log level.");
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_writer(std::io::stderr)
+        .compact()
+        .init();
+
+    let config = if let Some(config) = &opts.config {
+        verneuil::parse_configuration_string(config)
+            .ok_or_else(|| fresh_error!("failed to parse --config"))?
+    } else {
+        let value = std::env::var(verneuil::VERNEUIL_CONFIG_ENV_VAR)
+            .map_err(|e| chain_error!(e, "failed to fetch the value of VERNEUIL_CONFIG"))?;
+        verneuil::parse_configuration_string(&value)
+            .ok_or_else(|| fresh_error!("failed to parse VERNEUIL_CONFIG", %value))?
+    };
+
+    tracing::info!(?config, "parsed replication config");
+    verneuil::configure_replication(config.clone())
+        .map_err(|e| chain_error!(e, "failed to configure verneuil", ?config))?;
+
+    let directory_contents = std::fs::read(&opts.directory)
+        .map_err(|e| chain_error!(e, "failed to read directory file", path=?opts.directory))?;
+    let directory = verneuil::Directory::decode(&*directory_contents)
+        .map_err(|e| chain_error!(e, "failed to parse directory file", path=?opts.directory))?;
+    let snapshot = verneuil::Snapshot::new_with_default_targets(&directory)?;
+
+    if let Some(dst) = &opts.out {
+        let out_file = dst
+            .file_name()
+            .ok_or_else(|| fresh_error!("no file name in output path", ?dst))?;
+        let out_dir = dst
+            .parent()
+            .ok_or_else(|| fresh_error!("output path has no file name", ?dst))?;
+        let mut temp = tempfile::Builder::new()
+            .prefix(out_file)
+            .suffix(&format!(".{}.verneuil-restore-tmp", std::process::id()))
+            .tempfile_in(out_dir)
+            .map_err(|e| chain_error!(e, "failed to create temporary file", ?dst))?;
+
+        std::io::copy(&mut snapshot.as_read(), temp.as_file_mut()).map_err(|e| {
+            chain_error!(
+                e,
+                "failed to write snapshot contents to temporary file",
+                ?dst,
+                ?temp
+            )
+        })?;
+
+        temp.persist(dst).map_err(|e| {
+            chain_error!(
+                e,
+                "failed to persist temporary snapshot to out destination",
+                ?dst
+            )
+        })?;
+    } else {
+        std::io::copy(&mut snapshot.as_read(), &mut std::io::stdout())
+            .map_err(|e| chain_error!(e, "failed to write snapshot contents to stdout"))?;
+    }
+
+    Ok(())
+}
